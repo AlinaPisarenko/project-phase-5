a84bc1b5773904f4feaa3a6494a2a9b90c201298
Alina <pisarenko.alina@gmail.com>
2022-06-29 11:49:16 -0400

    created migrations, added seed data,set up authorization in rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+include ActionController::Cookies 
+before_action :authorize
+ 
+private 
+
+
+def authorize
+ @current_user = User.find_by(id: session[:user_id])
+ render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user 
+end
+
+def record_not_found(invalid)
+ render json: {error: "#{invalid.model} not found"}, status: 404
+end
+
+def record_invalid(invalid)
+render json: {errors: invalid.record.errors.full_messages}, status: 422
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :projects
+  resources :teams
+  resources :users, only: [:create, :destroy, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "/login", to: "sessions#create"
+  post "/signup", to: "users#create"
+  delete "/logout", to: "sessions#destroy" 
+  get "/me", to: "users#show"
 end
